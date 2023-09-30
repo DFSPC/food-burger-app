0f207b95d952ab2651169486288e240f3fd7e051
Daniel Fandino <dfscorreofinal@gmail.com>
2023-09-30 13:37:41 -0500

    feat: Add basic functionality app in adroid

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://food-burger.vercel.app/api",
+  cache: new InMemoryCache()
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,64 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonRefresher,
+  IonRefresherContent,
+  RefresherEventDetail, } from '@ionic/react';
 import './Home.css';
 
+import { useQuery, gql } from "@apollo/client";
+
+interface product {
+  _id: string,
+  title: string,
+  description: string,
+  price: number
+}
+
+const FILMS_QUERY = gql`
+  query Products {
+    products {
+      _id
+      title
+      description
+      img_url
+      price
+      featured
+    }
+  }
+`;
+
 const Home: React.FC = () => {
+  let { data, loading, error, refetch } = useQuery(FILMS_QUERY);
+  async function reloadData(event: CustomEvent<RefresherEventDetail>){
+    await refetch();
+    event.detail.complete();
+  } 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Food Burger</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <IonRefresher slot="fixed" onIonRefresh={reloadData}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+          {loading ? (<p>Loading...</p>) : ""}
+          {error ? (<pre>{error.message}</pre>) : ""}
+          {data?.products ? data.products.map((product:product) => (
+            <article key={product._id}>
+              <h3>{product.title}</h3>
+              <ul>
+                <li >Description: {product.description}</li>
+                <li >Price: {product.price}</li>
+              </ul>
+            </article>
+          )) : ""}
       </IonContent>
     </IonPage>
   );
