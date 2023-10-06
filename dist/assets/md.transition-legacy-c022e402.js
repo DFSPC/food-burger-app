930b8d0016a3f5221454dfcba29d872c51450bdd
Daniel Fandino <dfscorreofinal@gmail.com>
2023-10-06 15:18:18 -0500

    feat: Android build

--- dist/assets/md.transition-legacy-c022e402.js
+++ dist/assets/md.transition-legacy-c022e402.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-13fd237e.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.m,i=e.l}],execute:function(){
+System.register(["./index-legacy-28210930.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.m,i=e.l}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
